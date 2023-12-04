085c1806f75cbee7a5409b50ffa3c259bf281305
key-chi <114614158+key-chi@users.noreply.github.com>
2023-12-04 13:44:57 -0800

    fixeded'd lul

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import evaluate
 import trainer
+from PIL import Image
+from torchvision import transforms
 from utils import visualize
 
 import glob
@@ -26,8 +28,11 @@ def main():
 
     model = evaluate.get_model(args.checkpoint, num_templates=num_templates)
 
-    random_img = random.choice(list(test_loader))
-    print(random_img)
+    test_len = len(test_loader)
+
+    random_img_num = random.choice(range(test_len))
+    print(random_img_num)
+    print(test_loader.dataset[random_img_num])
 
     #random_img_cat = random.choice(TESTDATA_img_cat)
     #TESTDATA_img_list = glob.glob(f"{random_img_cat}/{pattern}")
@@ -38,11 +43,15 @@ def main():
     #print('lets test image: ' + random_img_path)
 
     with torch.no_grad():
-        dets = trainer.get_detections(model, random_img, templates, test_loader.dataset.rf,
+        dets = trainer.get_detections(model, test_loader.dataset[random_img_num], templates, test_loader.dataset.rf,
                                       test_loader.dataset.transforms, args.prob_thresh,
                                       args.nms_thresh, device=device)
         
-        visualize.visualize_bboxes()
+        # convert tensor to PIL image so we can perform resizing
+        image_path = f'{TESTDATA_img}{test_loader.dataset[random_img_num][1]}'
+        image = Image.open(image_path).convert('RGB')
+
+        visualize.visualize_bboxes(image, dets)
 
 
 if __name__ == '__main__':
